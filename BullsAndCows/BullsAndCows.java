2f9736006c1049c6865c12df585b70e92031cf09
Jackie Luo <jacquelineluo95@gmail.com>
2014-12-02 11:54:49 -0500

    Add BullsAndCows

---
+++ BullsAndCows/BullsAndCows.java
@@ -0,0 +1,14 @@
+//***************************************
+// This is the test class for the 
+// Bulls and Cows assignment
+//
+// Your application must use this main method
+//****************************************
+
+public class BullsAndCows {
+
+    public static void main(String[] args){
+        Game g = new Game();
+        g.playGame();
+    }   
+}
