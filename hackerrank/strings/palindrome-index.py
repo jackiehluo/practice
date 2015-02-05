663407dada1f9211defaf24189ef2f73a617022b
Jackie Luo <jacquelineluo95@gmail.com>
2015-02-05 11:03:02 -0500

    Update palindrome-index

--- hackerrank/strings/palindrome-index.py
+++ hackerrank/strings/palindrome-index.py
@@ -1,13 +1,19 @@
 def make_palindrome(text):
     reverse = text[::-1]
+    length = len(text)
     if text == reverse:
         return "-1"
     else:
-        for letter in range(len(text)):
-            new_text = text[:letter] + text[letter + 1:]
-            new_reverse = new_text[::-1]
-            if new_text == new_reverse:
-                return str(letter)
+        for letter in range(length):
+            if text[letter] != reverse[letter]:
+                first_text = text[:letter] + text[letter + 1:]
+                first_reverse = first_text[::-1]
+                second_text = text[:length - letter] + text[length - letter + 1:]
+                second_reverse = second_text[::-1]
+                if first_text == first_reverse:
+                    return str(letter)
+                elif second_text == second_reverse:
+                    return str(length - letter)
 
 test_cases = int(raw_input())
 
