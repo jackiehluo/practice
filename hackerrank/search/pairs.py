1c537957c84167b9d40845ec8798b60ee3cca0fd
Jackie Luo <jacquelineluo95@gmail.com>
2015-02-09 17:55:12 -0500

    Add pairs

---
+++ hackerrank/search/pairs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/py
+# Head ends here
+def pairs(a,k):
+    #a contains array of numbers and k is the value of difference
+    answer = 0
+    for i in range(len(a)):
+        for j in range(len(a)):
+            if a[j] - a[i] == k:
+                answer += 1
+    return answer
+# Tail starts here
+if __name__ == '__main__':
+    a = map(int, raw_input().strip().split(" "))
+    _a_size=a[0]
+    _k=a[1]
+    b = map(int, raw_input().strip().split(" "))
+    print pairs(b,_k)
