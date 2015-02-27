02fe4ed768ddac05770920a00ee0b2f2409b5cd1
Jackie Luo <jacquelineluo95@gmail.com>
2015-02-26 20:30:38 -0500

    Update antipalindromic-strings

--- hackerrank/implementation/antipalindromic-strings.py
+++ hackerrank/implementation/antipalindromic-strings.py
@@ -1,19 +1,29 @@
-from itertools import combinations_with_replacement
+from itertools import product
 
 def get_strings(n, m):
-    letters = list(map(chr, range(97, 97 + m + 1)))
-    total = 0
+    letters = list(map(chr, range(97, 97 + m)))
     count = 0
-    for i in combinations_with_replacement(letters, n):
-        total += 1
-        for j in range(1, n - 1):
-            if i[j] == i[j + 1]:
-                break
-            elif i[j - 1] == i[j + 1]:
-                break
-            else:
-                count += 1
-    print total
+    for i in product(letters, repeat = n):
+        if n <= 2:
+            if n == 1:
+                palindrome = False
+            elif n == 2:
+                if i[0] == i[1]:
+                    palindrome = True
+                else:
+                    palindrome = False
+        else:
+            for j in range(0, n - 2):
+                if i[j] == i[j + 1] or i[j + 1] == i[j + 2]:
+                    palindrome = True
+                    break
+                elif i[j] == i[j + 2]:
+                    palindrome = True
+                    break
+                else:
+                    palindrome = False
+        if palindrome == False:
+            count += 1
     return count
 
 test_cases = int(raw_input())
