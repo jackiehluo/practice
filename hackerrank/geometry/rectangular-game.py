b8abff6641a02fd0361259ff016c3a73f4442e41
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-01 13:39:47 -0500

    Update rectangular-game

--- hackerrank/geometry/rectangular-game.py
+++ hackerrank/geometry/rectangular-game.py
@@ -1,17 +1,3 @@
-def play_game(x, y, steps):
-    height = max(x)
-    width = max(y)
-    board = [[0 for b in range(width)] for b in range(height)]
-    largest = 0
-    for i in range(steps):
-        for j in range(x[i]):
-            for k in range(y[i]):
-                board[j][k] += 1
-                if board[j][k] > largest:
-                    largest = board[k][k]
-    number = sum(c.count(largest) for c in board)
-    return number
-
 steps = int(raw_input())
 x = []
 y = []
@@ -21,5 +7,5 @@ for step in range(steps):
     x.append(first)
     y.append(second)
 
-answer = play_game(x, y, steps)
+answer = min(x) * min(y)
 print answer
