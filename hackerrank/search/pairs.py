e9e270f4d3af0a28696ad4ffd8bb663f54ce2f49
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-01 12:57:17 -0500

    Update pairs

--- hackerrank/search/pairs.py
+++ hackerrank/search/pairs.py
@@ -1,11 +1,8 @@
-#!/usr/bin/py
-# Head ends here
 def pairs(a,k):
-    #a contains array of numbers and k is the value of difference
     numbers = set(a)
     answer = sum(1 for i in a if i - k in numbers)
     return answer
-# Tail starts here
+
 if __name__ == '__main__':
     a = map(int, raw_input().strip().split(" "))
     _a_size=a[0]
