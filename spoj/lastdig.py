f21b666755bd68a25e27a6c07c71a84c7c946771
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-05 02:08:18 -0500

    Update lastdig

--- spoj/lastdig.py
+++ spoj/lastdig.py
@@ -1,9 +1,24 @@
-def last_dig(a, b):
-    return str(a ** b)[-1:]
+digits = [[0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [2, 4, 8, 6],
+        [3, 9, 7, 1],
+        [4, 6, 4, 6],
+        [5, 5, 5, 5],
+        [6, 6, 6, 6],
+        [7, 9, 3, 1],
+        [8, 4, 2, 6],
+        [9, 1, 9, 1]]
 
 t = int(raw_input())
 
 for case in range(t):
     a, b = (int(x) for x in raw_input().split())
-    answer = last_dig(a, b)
-    print answer
+    a %= 10
+    rem = b % 4
+    if rem == 0:
+        if b == 0:
+            print "1"
+        else:
+            print digits[a][3]
+    else:
+        print digits[a][rem - 1]
