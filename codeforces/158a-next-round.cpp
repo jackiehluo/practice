34b6c9ed4af6070ec2246a961394ce53675a9c90
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-06 11:57:44 -0500

    Add 158a-next-round

---
+++ codeforces/158a-next-round.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, k, max;
+    cin>>n>>k;
+    int p[50];
+    for(int i = 0; i < n; i++)
+    {
+    	cin>>p[i];
+    	if(i + 1 == k)
+    		max = p[i];
+    }
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+    	if(p[i] >= max && p[i] > 0)
+    		count++;
+    }
+    cout<<count;
+    return 0;
+}
