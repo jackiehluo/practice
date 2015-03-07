23e4cd37fca0faad565bd18bb9e4e89cbfa0f25b
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-07 01:35:31 -0500

    Add 131a-caps-lock

---
+++ codeforces/131a-caps-lock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    char u;
+    bool c = true;
+    cin >> s;
+
+    for(int i = 1; i < s.length(); i++)
+    {
+        if(islower(s[i]))
+        {
+            c = false;
+        }
+    }
+
+    if(c == true)
+    {
+        for(int j = 0; j < s.length(); j++)
+        {
+            if(islower(s[j]))
+                u = toupper(s[j]);
+            else
+                u = tolower(s[j]);
+            cout << u;
+        }
+    }
+    else
+        cout << s;
+    return 0;
+}
