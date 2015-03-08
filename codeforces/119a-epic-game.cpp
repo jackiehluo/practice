e725696fda60e6aadc78e1ccdfd088256fdca91a
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-08 15:29:23 -0400

    Add 119a-epic-game

---
+++ codeforces/119a-epic-game.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int gcd;
+    for(int i = 1; i <= a && i <= b; i++)
+	{
+	    if(a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+    return gcd;
+}
+
+int main()
+{
+	int a, b, n;
+	cin >> a >> b >> n;
+
+	while(true)
+	{
+		n -= gcd(a, n);
+		if(n == 0)
+        {
+			cout << "0" << endl;
+            break;
+        }
+		n -= gcd(b, n);
+        if(n == 0)
+        {
+			cout << "1" << endl;
+            break;
+        }
+	}
+	return 0;
+}
