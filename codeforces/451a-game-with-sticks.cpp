2e43f0db0f4f4fd815a383019d84c94d7e5ff9f9
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-10 10:26:08 -0400

    Add 451a-game-with-sticks

---
+++ codeforces/451a-game-with-sticks.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int i = a * b;
+	int t = 0;
+
+	while(i > 0)
+	{
+		t++;
+		i -= a + b - 1;
+		a--;
+		b--;
+	}
+
+	if(t % 2 == 0)
+		cout << "Malvika" << endl;
+	else
+		cout << "Akshat" << endl;
+	return 0;
+}
