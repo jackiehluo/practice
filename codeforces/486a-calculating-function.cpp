9e338896755f1ca6f4026a25a2e99389b04e8e8d
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-10 11:04:27 -0400

    Add 486a-calculating-function

---
+++ codeforces/486a-calculating-function.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long n, a;
+	cin >> n;
+	
+    if(n % 2 == 0)
+        a = n / 2;
+    else
+        a = ((n + 1) / 2) * (-1);
+
+	cout << a << endl;
+	return 0;
+}
