f408abcf8ca53cfe0e1ecad8714973456143e3e2
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-10 11:12:45 -0400

    Add 500a-new-year-transportation

---
+++ codeforces/500a-new-year-transportation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, t;
+	cin >> n >> t;
+	int c[n];
+	int p = 0;
+	
+	for(int i = 0; i < n; i++)
+	{
+		cin >> c[i];
+	}
+
+	while(p < t - 1)
+	{
+		p += c[p];
+	}
+
+	if(p == t - 1)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+	return 0;
+}
