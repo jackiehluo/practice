a7befd1197db1f65e3e46a4c11165b1004556ac2
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-12 21:34:49 -0400

    Add OpenID to login

--- flask-microblog/app/views.py
+++ flask-microblog/app/views.py
@@ -28,6 +28,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', 
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
