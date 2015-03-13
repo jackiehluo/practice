2d01d546ced8807b4c5cf929ec6a34f55dc898a1
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-13 11:16:15 -0500

    Add database, working on configparser error

---
+++ flask-microblog/app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.model):
+	id = db.Column(db.Integer, primary_key = True)
+	nickname = db.Column(db.String(64), index = True, unique = True)
+	email = db.Column(db.String(120), index = True, unique = True)
+
+	def __repr__(self):
+		return '<User %r' % (self.nickname)
--- flask-microblog/config.py
+++ flask-microblog/config.py
@@ -1,3 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Eugenides_14'
 
