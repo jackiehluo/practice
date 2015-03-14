4f25db323f1b3cd0440556dbfbd6b680b380d5b1
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-14 16:18:45 -0500

    Add 513a-game

---
+++ codeforces/500s/513a-game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, k1, k2;
+    cin >> n1 >> n2 >> k1 >> k2;
+    int t = 1;
+
+    while(n1 > 0 && n2 > 0)
+    {
+        if(t % 2 == 1)
+            n1 -= 1;
+        else
+            n2 -= 1;
+        t++;
+    }
+
+    if(n1 <= 0)
+        cout << "Second" << endl;
+    else
+        cout << "First" << endl;        
+}
