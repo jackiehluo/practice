2017ea89ba8d1e413d3d7d2187d2bac164dfb04b
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-14 16:31:49 -0500

    Add 510a-fox-and-snake

---
+++ codeforces/500s/510a-fox-and-snake.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int r = 1;
+    bool l = false;
+
+    while(r <= n)
+    {
+        if(r % 2 == 1)
+        {
+            for(int i = 0; i < m; i++)
+                cout << "#";
+            cout << endl;
+        }
+        else if(r % 2 == 0 && l == false)
+        {
+            for(int j = 0; j < m - 1; j++)
+                cout << ".";
+            cout << "#" << endl;
+            l = true;
+        }
+        else
+        {
+            cout << "#";
+            for(int j = 0; j < m - 1; j++)
+                cout << ".";
+            cout << endl;
+            l = false;
+        }
+        r++;
+    }
+    return 0;     
+}
