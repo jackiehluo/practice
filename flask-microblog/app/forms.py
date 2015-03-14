91c42317518d2aad3121fb504358768b4cbe8fe7
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-13 21:52:37 -0500

    Finish database model

--- flask-microblog/app/forms.py
+++ flask-microblog/app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    openid = StringField('openid', validators = [DataRequired()])
+    openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
