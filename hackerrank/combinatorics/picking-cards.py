c6575eb5bd442f33ae8cdf08eb4a684f4fe42ea3
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-24 00:07:36 -0400

    Update picking-cards

--- hackerrank/combinatorics/picking-cards.py
+++ hackerrank/combinatorics/picking-cards.py
@@ -1,17 +1,14 @@
-def count(cards, m):
-    count = 0
-    for value in cards:
-        if value <= m:
-            count += 1
-    return count
+def count(cards):
+    c = sorted(cards)
+    ways = 1
+    for i in range(len(cards), 0, -1):
+        ways *= i - c[i - 1]
+        ways %= 1000000007
+    return ways
 
 t = int(raw_input())
 
 for case in range(t):
     n = int(raw_input())
     cards = [int(x) for x in raw_input().split()]
-    total = 1;
-    for i in range(len(cards)):
-        ways = max(0, count(cards, i) - i)
-        total = (total * ways) % 1000000007
-    print total
+    print count(cards)
