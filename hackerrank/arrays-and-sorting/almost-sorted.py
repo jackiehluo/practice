e4d2d64db13b048676f894d467c297c4c8af6777
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-25 14:04:29 -0400

    Update almost-sorted

--- hackerrank/arrays-and-sorting/almost-sorted.py
+++ hackerrank/arrays-and-sorting/almost-sorted.py
@@ -1,42 +1,28 @@
-def swap(numbers):
-    size = len(numbers)
-    sorted_numbers = sorted(numbers)
-    for i in range(size):
-        for j in range(size):
-            if i != j:
-                temp = list(numbers)
-                temp[i], temp[j] = temp[j], temp[i]
-                if temp == sorted_numbers:
-                    print "yes"
-                    print "swap " + str(i + 1) + " " + str(j + 1)
-                    return True
-    return False
+def sort(n, d):
+    sd = sorted(d)
+    if d == sd:
+        return True
+    index = []
+    for i in range(n):
+        if d[i] != sd[i]:
+            index.append(i + 1)
+    if len(index) == 2:
+        return index
+    elif index:
+        first, last = index[0], index[-1]
+        sublist = d[first - 1:last]
+        if sublist[::-1] == sd[first - 1:last]:
+            return first, last
 
-def reverse(numbers):
-    size = len(numbers)
-    sorted_numbers = sorted(numbers)
-    for k in range(size):
-        for l in range(size):
-            if k < l:
-                temp = list(numbers)
-                temp[k:(l + 1)] = reversed(temp[k:(l + 1)])
-                if temp == sorted_numbers:
-                    print "yes"
-                    print "reverse " + str(k + 1) + " " + str(l + 1)
-                    return True
-    return False
-
-size = int(raw_input())
-numbers = [int(x) for x in raw_input().split()]
-match = False
-sorted_numbers = sorted(numbers)
-
-if numbers == sorted_numbers:
-    match = True
+n = int(raw_input())
+d = [int(x) for x in raw_input().split()]
+answer = sort(n, d)
+if answer:
     print "yes"
+    if type(answer) is not bool:
+        if type(answer) is list:
+            print "swap", " ".join(map(str, answer))
+        elif type(answer) is tuple:
+            print "reverse", " ".join(map(str, answer))
 else:
-    match = swap(numbers)
-    if not match:
-        match = reverse(numbers)
-        if not match:
-            print "no"
+    print "no"
