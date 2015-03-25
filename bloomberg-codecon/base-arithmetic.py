b1053dc0718004f3188290eb2fe530928f3d5302
Jackie Luo <jacquelineluo95@gmail.com>
2015-03-25 17:17:16 -0400

    Add base-arithmetic, starting Bloomberg Codecon

---
+++ bloomberg-codecon/base-arithmetic.py
@@ -0,0 +1,15 @@
+def find_base(x):
+    bases = {"1" : 2, "2" : 3, "3" : 4, "4" : 5,
+            "5" : 6, "6" : 7, "7" : 8, "8" : 9,
+            "9" : 10, "A" : 11, "B" : 12, "C" : 13,
+            "D" : 14, "E" : 15, "F" : 16}
+    largest = "1"
+    for char in x:
+        if char > largest:
+            largest = char
+    return bases[largest]
+
+a = raw_input()
+b = raw_input()
+answer = int(a, find_base(a)) + int(b, find_base(b))
+print answer
