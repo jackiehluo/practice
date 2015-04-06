fd89b3ab45de65d0f3c4d1f55c7ceaefea860120
Jackie Luo <jacquelineluo95@gmail.com>
2015-04-06 15:17:44 -0400

    Add code test for Yodle

---
+++ interviews/yodle/triangle.py
@@ -0,0 +1,13 @@
+def max_sum(rows):
+    for i in range(len(rows) - 2, -1, -1):
+        for j in range(i + 1):
+            rows[i][j] += max([rows[i + 1][j], rows[i + 1][j + 1]])
+    return rows[0][0]
+
+f = open('triangle.txt', 'r')
+rows = []
+
+for line in f:
+    rows.append([int(x) for x in line.split()])
+
+print max_sum(rows)
