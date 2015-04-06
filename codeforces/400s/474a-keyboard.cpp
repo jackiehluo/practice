bee4676e363c1f30cdd2b79961bdb817f78aaeec
Jackie Luo <jacquelineluo95@gmail.com>
2015-04-05 21:38:20 -0400

    Update 474a-keyboard

--- codeforces/400s/474a-keyboard.cpp
+++ codeforces/400s/474a-keyboard.cpp
@@ -1,30 +1,24 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int index(const char *array, int size, char *c)
-{
-    const char* end = array + size;
-    const char* n = find(array, end, c);
-    return (end == n)? -1 : (n-array);
-}
 
 int main()
 {
-    const char *k[30] = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-                    "a", "s", "d", "f", "g", "h", "j", "k", "l", ";",
-                    "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"};
-    char *d;
+    vector<char> k {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+                    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+                    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'};
+    char d;
     string m;
     int p;
     cin >> d;
     cin >> m;
 
-    if(d == "R")
+    if(d == 'R')
     {
         for(int i = 0; i < m.length(); i++)
         {
-            p = index(k, 30, k[i]) - 1;
+            p = find(k.begin(), k.end(), m[i]) - k.begin() - 1;
             cout << k[p];
         }
     }
@@ -32,7 +26,7 @@ int main()
     {
         for(int j = 0; j < m.length(); j++)
         {
-            p = index(k, 30, k[j]) + 1;
+            p = find(k.begin(), k.end(), m[j]) - k.begin() + 1;
             cout << k[p];
         }
     }
