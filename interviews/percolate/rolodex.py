a1b84f7616557586646f0eb51cb58e763b9563e6
Jackie Luo <jacquelineluo95@gmail.com>
2015-04-10 12:54:40 -0400

    Reformat code

--- interviews/percolate/rolodex.py
+++ interviews/percolate/rolodex.py
@@ -1,20 +1,21 @@
 from collections import OrderedDict
 import json
 
-f = open('data.in', 'r')
-line_count = 0
-output = OrderedDict()
-output['entries'] = []
-output['errors'] = []
+def rolodex(input):
 
-for line in f:
-    entry = line.split(', ')
-    info = OrderedDict()
-    if len(entry) == 5:
-        if (entry[0].isalpha() and not entry[1].isdigit() and not
-            entry[2].isalpha() and len(entry[2]) == 14 and not
-            entry[3].isdigit() and not entry[4].isalpha() and
-            len(entry[4]) == 6):
+    f = open(input, 'r')
+    line_count = 0
+    output = OrderedDict()
+    output['entries'] = []
+    output['errors'] = []
+
+    for line in f:
+        entry = line.split(', ')
+        info = OrderedDict()
+        if (len(entry) == 5 and entry[0].isalpha() and not
+            entry[1].isdigit() and not entry[2].isalpha() and 
+            len(entry[2]) == 14 and not entry[3].isdigit() and not
+            entry[4].isalpha() and len(entry[4]) == 6):
             info['color'] = entry[3]
             info['firstname'] = entry[1]
             info['lastname'] = entry[0]
@@ -25,22 +26,20 @@ for line in f:
             info['phonenumber'] = '-'.join([num[:3], num[3:6], num[6:]])
             info['zipcode'] = entry[4][:-1]
             output['entries'].append(info)
-        elif (not entry[0].isdigit() and entry[1].isalpha() and
-            entry[2].isdigit() and len(entry[2]) == 5 and not
-            entry[3].isalpha() and len(entry[3]) == 12 and not
-            entry[4].isdigit()):
+        elif (len(entry) == 5 and not entry[0].isdigit() and
+            entry[1].isalpha() and entry[2].isdigit() and
+            len(entry[2]) == 5 and not entry[3].isalpha() and
+            len(entry[3]) == 12 and not entry[4].isdigit()):
             info['color'] = entry[4][:-1]
             info['firstname'] = entry[0]
             info['lastname'] = entry[1]
             info['phonenumber'] = '-'.join(entry[3].split())
             info['zipcode'] = entry[2]
             output['entries'].append(info)
-        else:
-            output['errors'].append(line_count)
-    elif len(entry) == 4:
-        if (not entry[0].isdigit() and not entry[1].isdigit() and
-            entry[2].isdigit() and len(entry[2]) == 5 and not
-            entry[3].isalpha() and len(entry[3]) == 13):
+        elif (len(entry) == 4 and not entry[0].isdigit() and not
+            entry[1].isdigit() and entry[2].isdigit() and
+            len(entry[2]) == 5 and not entry[3].isalpha() and
+            len(entry[3]) == 13):
             info['color'] = entry[1]
             name = entry[0].split()
             info['firstname'] = ' '.join(name[:-1])
@@ -50,11 +49,13 @@ for line in f:
             output['entries'].append(info)
         else:
             output['errors'].append(line_count)
-    else:
-        output['errors'].append(line_count)
-    line_count += 1
+        line_count += 1
 
-output['entries'] = sorted(output['entries'], key=lambda k: (k['lastname'], k['firstname']))
+    output['entries'] = sorted(output['entries'], key=lambda k: (k['lastname'], k['firstname']))
 
-with open('result.out', 'w') as newfile:
-    json.dump(output, newfile, indent = 2)
+    with open('result.out', 'w') as newfile:
+        json.dump(output, newfile, indent = 2)
+
+if __name__ == '__main__':
+
+    rolodex('data.in')
