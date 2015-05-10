c427a30db06e02110bfbdd0552ac6802a6edd5a0
Jackie Luo <jacquelineluo95@gmail.com>
2015-05-10 14:36:15 -0400

    Update poker-hands, off by one

--- project-euler/51-100/poker-hands.py
+++ project-euler/51-100/poker-hands.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 def is_royal_flush(hand):
-    v = set(["10", "J", "Q", "K", "A"])
+    v = set(["T", "J", "Q", "K", "A"])
     s = hand[0][-1]
     for c in hand:
         if c[-1] != s:
@@ -13,8 +13,8 @@ def is_royal_flush(hand):
     return True
 
 def is_straight_flush(hand):
-    v = {"2", : 2, "3" : 3, "4" : 4, "5" : 5,
-        "6" : 6, "7" : 7, "8" : 8, "9" : 9, "10" : 10,
+    v = {"2" : 2, "3" : 3, "4" : 4, "5" : 5,
+        "6" : 6, "7" : 7, "8" : 8, "9" : 9, "T" : 10,
         "J" : 11, "Q" : 12, "K" : 13, "A" : 14}
     n = []
     s = hand[0][-1]
@@ -59,8 +59,8 @@ def is_flush(hand):
     return True
 
 def is_straight(hand):
-    v = {"2", : 2, "3" : 3, "4" : 4, "5" : 5,
-        "6" : 6, "7" : 7, "8" : 8, "9" : 9, "10" : 10,
+    v = {"2" : 2, "3" : 3, "4" : 4, "5" : 5,
+        "6" : 6, "7" : 7, "8" : 8, "9" : 9, "T" : 10,
         "J" : 11, "Q" : 12, "K" : 13, "A" : 14}
     n = []
     for c in hand:
@@ -103,14 +103,14 @@ def is_one_pair(hand):
     return False
 
 def winning_hand(a, b):
-    v = {"2", : 2, "3" : 3, "4" : 4, "5" : 5,
-        "6" : 6, "7" : 7, "8" : 8, "9" : 9, "10" : 10,
+    v = {"2" : 2, "3" : 3, "4" : 4, "5" : 5,
+        "6" : 6, "7" : 7, "8" : 8, "9" : 9, "T" : 10,
         "J" : 11, "Q" : 12, "K" : 13, "A" : 14}
     a_values = []
     b_values = []
     for i in range(5):
         a_values.append(v[a[i][:-1]])
-        b_values.append(v[a[i][:-1]])
+        b_values.append(v[b[i][:-1]])
     a_values.sort(reverse = True)
     b_values.sort(reverse = True)
     for j in range(5):
@@ -131,8 +131,8 @@ for line in f:
     game.append([first_hand, second_hand])
 
 for g in game:
-    g[0] == first
-    g[1] == second
+    first = g[0]
+    second = g[1]
     if is_royal_flush(first) or is_royal_flush(second):
         if is_royal_flush(first) and is_royal_flush(second):
             winner = winning_hand(first, second)
@@ -155,3 +155,88 @@ for g in game:
             player_one += 1
         else:
             player_two += 1
+    elif is_four_of_a_kind(first) or is_four_of_a_kind(second):
+        if is_four_of_a_kind(first) and is_four_of_a_kind(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_four_of_a_kind(first):
+            player_one += 1
+        else:
+            player_two += 1
+    elif is_full_house(first) or is_full_house(second):
+        if is_full_house(first) and is_full_house(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_full_house(first):
+            player_one += 1
+        else:
+            player_two += 1
+    elif is_flush(first) or is_flush(second):
+        if is_flush(first) and is_flush(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_flush(first):
+            player_one += 1
+        else:
+            player_two += 1
+    elif is_straight(first) or is_straight(second):
+        if is_straight(first) and is_straight(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_straight(first):
+            player_one += 1
+        else:
+            player_two += 1
+    elif is_three_of_a_kind(first) or is_three_of_a_kind(second):
+        if is_three_of_a_kind(first) and is_three_of_a_kind(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_three_of_a_kind(first):
+            player_one += 1
+        else:
+            player_two += 1
+    elif is_two_pairs(first) or is_two_pairs(second):
+        if is_two_pairs(first) and is_two_pairs(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_two_pairs(first):
+            player_one += 1
+        else:
+            player_two += 1
+    elif is_one_pair(first) or is_one_pair(second):
+        if is_one_pair(first) and is_one_pair(second):
+            winner = winning_hand(first, second)
+            if winner == first:
+                player_one += 1
+            else:
+                player_two += 1
+        elif is_one_pair(first):
+            player_one += 1
+        else:
+            player_two += 1
+    else:
+        winner = winning_hand(first, second)
+        if winner == first:
+            player_one += 1
+        else:
+            player_two += 1
+
+print player_one, player_two
