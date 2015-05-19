9bdf07586ef46a4fa85889f5759e09e28beaf0cc
Jackie Luo <jacquelineluo95@gmail.com>
2015-05-19 00:25:38 -0500

    Start 520a-pangram

---
+++ codeforces/500s/520a-pangram.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctype.h>
+#include <map>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    map<char, int> counts;
+    cin >> n >> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        char c = putchar(tolower(s[i]));
+        if (counts[c])
+            counts[c]++;
+        else
+            counts[c] = 1;
+    }
+
+    if (counts.size() == 26)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
