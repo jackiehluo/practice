feb02657766811aaa1cdc61975c9375ae1b8b2d7
Jackie Luo <jacquelineluo95@gmail.com>
2015-06-16 12:59:38 -0400

    Add 515a-drazil-and-date

---
+++ codeforces/500s/515a-drazil-and-date.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, steps;
+    cin >> x >> y >> steps;
+    int shortest = abs(x) + abs(y);
+
+    if (shortest > steps or shortest % 2 != steps % 2)
+        cout << "No" << endl;
+    else
+        cout << "Yes" << endl;
+    return 0;
+}
