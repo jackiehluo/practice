18850726381471c617a3efa3d5be8b3a967f8d04
Jackie Luo <jacquelineluo95@gmail.com>
2015-06-29 13:54:34 -0400

    Update 327a-flipping-game

--- codeforces/300s/327a-flipping-game.cpp
+++ codeforces/300s/327a-flipping-game.cpp
@@ -1,43 +1,28 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
     int n, t;
+    int count = 0, extra = 0, max_extra = -1;
     cin >> n;
-    vector<int> numbers;
-
-    for (int i = 0; i < n; i++)
+    
+    while (n--)
     {
         cin >> t;
-        if (t == 0)
-            numbers.push_back(1);
-        else
-            numbers.push_back(0);
-    }
-
-    bool p = false;
-    int cur = 0, max = 0;
-    int v;
-
-    for (int i = 0; i < n; i++)
-    {
-        v = cur + numbers[i];
-        if (v > 0)
+        if (t == 1)
         {
-            cur = v;
-            p = true;
+            count++;
+            if (extra > 0)
+                extra--;
         }
         else
-            cur = 0;
-        if (cur > max)
-            max = cur;
+        {
+            extra++;
+            if (extra > max_extra)
+                max_extra = extra;
+        }
     }
-
-    if (p == false)
-        max = max_element(begin(numbers), end(numbers));
-
-    cout << max << endl;
+    cout << count + max_extra << endl;
     return 0;
 }
