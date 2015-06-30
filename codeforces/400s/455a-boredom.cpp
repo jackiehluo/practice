1a9d7432fba29aac915b4e80c5e8b88446906edb
Jackie Luo <jacquelineluo95@gmail.com>
2015-06-30 16:30:02 -0400

    Add 455a-boredom

---
+++ codeforces/400s/455a-boredom.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    const int N = 1000000;
+    int counts[N];
+    long long points[N];
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        counts[tmp]++;
+    }
+
+    points[0] = 0;
+
+    for (int i = 1; i < N; i++)
+    {
+        points[i] = (long long)i * counts[i];
+        if (i - 2 >= 0)
+            points[i] += points[i - 2];
+        if (points[i - 1] > points[i])
+            points[i] = points[i - 1];
+    }
+
+    cout << points[N - 1] << endl;
+    return 0;
+}
