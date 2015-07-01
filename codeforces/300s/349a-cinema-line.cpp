a1ae9b44da6fdb87c50b1d291a9461eb90808ec9
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-01 13:49:41 -0400

    Update 349a-cinema-line

--- codeforces/300s/349a-cinema-line.cpp
+++ codeforces/300s/349a-cinema-line.cpp
@@ -5,18 +5,38 @@ int main()
 {
     int n, t;
     cin >> n;
-    int change = 0;
+    int a = 0, b = 0;
     string answer = "YES";
 
     for (int i = 0; i < n; i++)
     {
         cin >> t;
         if (t == 25)
-            change += 25;
-        else if (change - (t - 25) < 0)
+            a += 1;
+        else if (t == 50)
         {
-            answer = "NO";
-            break;
+            b += 1;
+            a -= 1;
+            if (a < 0)
+            {
+                answer = "NO";
+                break;
+            }
+        }
+        else
+        {
+            if (a >= 1 and b >= 1)
+            {
+                a -= 1;
+                b -= 1;
+            }
+            else if (a >= 3)
+                a -= 3;
+            else
+            {
+                answer = "NO";
+                break;
+            }
         }
     }
 
