798647d82f11c23413f53a64df2d9b6bfb782a11
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-07 23:26:58 -0400

    Add 385a-bear-and-raspberry

---
+++ codeforces/300s/385a-bear-and-raspberry.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, c;
+    cin >> n >> c;
+    int d[n], t(0);
+
+    for (int i = 0; i < n; i++)
+      cin >> d[i];
+    
+    for (int i = 0; i < n - 1; i++)
+        t = max(t, d[i] - d[i + 1] - c);
+
+    cout << t << endl;
+    return 0;
+}
