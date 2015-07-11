0909006eb34c0bb5b636a260b71227b05299b2d0
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-11 13:41:08 -0400

    Add 313b-ilya-and-queries

---
+++ codeforces/300s/313b-ilya-and-queries.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int c[s.length()], t(0);
+    c[0] = 0;
+
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (s[i] == s[i - 1])
+            t++;
+        c[i] = t;        
+    }
+
+    int q, l, r;
+    cin >> q;
+
+    for (int i = 0; i < q; i++)
+    {
+        cin >> l >> r;
+        l--, r--;
+        cout << c[r] - c[l] << endl;
+    }
+    return 0;
+}
