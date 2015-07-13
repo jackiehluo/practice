096759991f857c40fb152cf47706a78a6b03b0e5
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-13 10:31:58 -0400

    Add 535b-tavas-and-saddas

---
+++ codeforces/500s/535b-tavas-and-saddas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m(1), t;
+    cin >> n;
+
+    while (n)
+    {
+        if (n % 10 == 4) t += m; 
+        else t += 2 * m;
+        n /= 10, m *= 2;
+    }
+
+    cout << t << endl;
+    return 0;
+}
