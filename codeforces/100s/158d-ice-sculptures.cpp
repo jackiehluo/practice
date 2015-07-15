dbbe7a75838ad92c7175a4e7f56aa4b7f7c17aa0
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-15 08:31:55 -0400

    Add 158d-ice-sculptures

---
+++ codeforces/100s/158d-ice-sculptures.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int n, t(0);
+    cin >> n;
+    int a[n];
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        t += a[i];
+    }
+    
+    for (int j = 1; j <= n; j++)
+    {
+        if (n % j != 0) continue;
+        if (n / j < 3) continue;
+        for (int k = 1; k <= j; k++)
+        {
+            int c = 0;
+            for (int i = k; i <= n; i += j)
+                c += a[i]; 
+            if(c > t) t = c;   
+        }
+    }   
+
+    cout << t << endl;
+    return 0;
+}
