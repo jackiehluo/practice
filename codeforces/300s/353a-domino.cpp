49d5be107bdd996c74447872a33379fa04835ac1
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-16 23:43:52 -0400

    Add 353a-domino

---
+++ codeforces/300s/353a-domino.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    int n, l(0), r(0);
+    cin >> n;
+    bool s(false);
+    for (int x, y; cin >> x >> y;)
+    {
+        if (n == 1 and x % 2 + y % 2 == 1)
+        {
+            cout << -1;
+            return 0;
+        }
+        if (y % 2 + x % 2 == 1)
+            s = true;
+        l += x;
+        r += y;
+    }
+    if (l % 2 == 0 and r % 2 == 0) cout << 0 << endl;
+    else if (l % 2 == 1 and r % 2 == 1 and s) cout << 1 << endl;
+    else cout << -1 << endl;
+    return 0;
+}
