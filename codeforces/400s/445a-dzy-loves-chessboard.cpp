43e2a7aab5636e6d734cb3009e4efe26566a6107
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-18 16:11:41 -0400

    Add 445a-dzy-loves-chessboard

---
+++ codeforces/400s/445a-dzy-loves-chessboard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int n, m;
+    cin >> n >> m;
+    char s[m];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        for (int j = 0; j < m; j++)
+            if (s[j] == '.') cout << ((i + j) % 2 ? 'W' : 'B');
+            else cout << '-';
+        cout << endl;
+    }
+    return 0;
+}
