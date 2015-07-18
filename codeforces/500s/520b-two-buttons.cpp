5e31fa6efcdf771e31f80d462698f0afbe41225e
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-18 16:20:57 -0400

    Add 520b-two-buttons

---
+++ codeforces/500s/520b-two-buttons.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m, w(0);
+    cin >> n >> m;
+    while (n != m)
+    {
+        if (m > n)
+            if (m % 2 == 0) m /= 2, w++;
+            else m++, w++;
+        else if (n > m) m++, w++;
+    }
+    cout << w << endl;
+    return 0;
+}
