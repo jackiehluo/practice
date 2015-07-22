ec9918db36c348739d4525e8c29979fbf7f69af6
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-22 12:51:58 -0400

    Add 426a-sereja-and-mugs

---
+++ codeforces/400s/426a-sereja-and-mugs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, s, m[100];
+    cin >> n >> s;
+    for (int i = 0; i < n; i++) cin >> m[i];
+    sort(m, m + n);
+    for (int i = 0; i < n - 1; i++) s -= m[i];
+    cout << (s >= 0 ? "YES" : "NO") << endl;
+    return 0;
+}
