059e542c5cc5f099961aac0acfdf19cfadcd90f9
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-30 14:08:18 -0400

    Update sherlock-and-moving-tiles, passing all tests

--- hackerrank/mathematics/fundamentals/sherlock-and-moving-tiles.py
+++ hackerrank/mathematics/fundamentals/sherlock-and-moving-tiles.py
@@ -2,5 +2,6 @@ from math import sqrt
 
 l, s1, s2 = map(int, raw_input().split())
 q = int(raw_input())
+v = abs(s1 - s2) / sqrt(2)
 for _ in range(q):
-    print 2 * (l - sqrt(int(raw_input()))) / abs(s1 - s2) / sqrt(2)
+    print format((l - sqrt(int(raw_input()))) / v, '10.22f')
