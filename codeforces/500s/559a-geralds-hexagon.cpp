d8f86619a97ac1fbb1a6c3f590b04dd15b3e0dc8
Jackie Luo <jacquelineluo95@gmail.com>
2015-08-06 16:39:07 -0400

    Add 559a-geralds-hexagon

---
+++ codeforces/500s/559a-geralds-hexagon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << (a + b + f) * (a + b + f) - b * b - d * d - f * f << endl;
+    return 0;
+}
