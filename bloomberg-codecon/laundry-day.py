7d8f988ef08459ae7bc2862c8dacc85f7eef4f6d
Jackie Luo <jacquelineluo95@gmail.com>
2015-08-12 13:16:51 -0400

    Update laundry-day, still failing judge test cases :confused:

--- bloomberg-codecon/laundry-day.py
+++ bloomberg-codecon/laundry-day.py
@@ -1,16 +1,12 @@
 import sys
 from collections import Counter
 
-clothing = []
-
-for item in sys.stdin:
-    clothing.append(item)
-
+clothing = [item.strip() for item in sys.stdin]
 counts = Counter(clothing)
 
 for k, v in sorted(counts.items()):
-    if 'sock' in k:
-        if v / 2 > 0:
+    if "sock" in k:
+        if v > 1:
             print str(v / 2) + '|' + k
         if v % 2 == 1:
             print '0' + '|' + k
