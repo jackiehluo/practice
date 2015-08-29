04c72dbc4e7a782f933dc63731ab4b51d08a7a3d
Jackie Luo <jacquelineluo95@gmail.com>
2015-08-29 18:47:56 -0500

    Add shorter bst-insertion, passing all tests

--- hackerrank/data-structures/trees/bst-insertion.cpp
+++ hackerrank/data-structures/trees/bst-insertion.cpp
@@ -1,20 +1,6 @@
-node * insert(node * root, int value)
+node* insert(node*& root, int value)
 {
-    if (value < root->data)
-        if (!root->left)
-        {
-            node* n = new node;
-            n->data = value;
-            root->left = n;
-        }
-        else insert(root->left, value);
-    else if (value > root->data)
-        if (!root->right)
-        {
-            node* n = new node;
-            n->data = value;
-            root->right = n;
-        }
-        else insert(root->right, value);
-   return root;
+    if (root) insert(value < root->data ? root->left : root->right, value);
+    else root = new node, root->data = value;
+    return root;
 }
