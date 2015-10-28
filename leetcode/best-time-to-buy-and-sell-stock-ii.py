b5a424b12d439233389a7272d0e59d53ed611b17
Jackie Luo <jacquelineluo95@gmail.com>
2015-10-27 21:49:22 -0400

    Add best-time-to-buy-and-sell-stock-ii

---
+++ leetcode/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
