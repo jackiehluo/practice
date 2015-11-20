294dad7c939fe36f14c81b574c2efde7106bd5f8
Jackie Luo <jacquelineluo95@gmail.com>
2015-11-19 23:52:29 -0500

    Add lowest-common-ancestor-of-binary-tree

---
+++ leetcode/lowest-common-ancestor-of-binary-tree.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
