21de0f332c3d34dd17bd69e6d5a0aa334510b306
Jackie Luo <jacquelineluo95@gmail.com>
2016-01-01 21:22:41 -0600

    Update maximum-product-of-word-lengths

--- leetcode/maximum-product-of-word-lengths.py
+++ leetcode/maximum-product-of-word-lengths.py
@@ -1,8 +1,12 @@
 class Solution(object):
     def maxProduct(self, words):
+        keys = [0] * len(words)
+        for i in range(len(words)):
+            for c in words[i]:
+                keys[i] |= 1 << ord(c) - ord("a")
         max_product = 0
         for i in range(len(words)):
             for j in range(i + 1, len(words)):
-                if set(words[i]).isdisjoint(words[j]):
+                if (keys[i] & keys[j]) == 0:
                     max_product = max(max_product, len(words[i]) * len(words[j]))
         return max_product
